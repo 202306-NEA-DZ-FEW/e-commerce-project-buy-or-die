db1ab844ea82b257024be87ca756314ae57bb647
takidilmi <angeldilmi@gmail.com>
2023-09-28 00:56:34 +0200

    fix(productcard rating): fix the rating of the product card
    fix the rating goingthrough the card when price number is too big
    
    Closes #22

--- src/components/Cards/ProductCard.jsx
+++ src/components/Cards/ProductCard.jsx
@@ -24,22 +24,24 @@ const ProductCard = ({
         </div>
         <div className="mt-4 px-5 pb-5">
           <div>
-            <h5 className="text-xl tracking-tight text-slate-900">{title}</h5>
+            <h5 className="text-xl text-center tracking-tight text-slate-900">
+              {title}
+            </h5>
           </div>
-          <div className="mt-2 mb-5 flex items-center justify-between">
+          <div className="mt-2 mb-5 flex flex-col items-center justify-between">
             <p>
               <span className="text-3xl font-bold text-slate-900">
                 ${price}
               </span>
-              <span className="text-sm text-red-600 line-through">
+              <span className="text-sm justify-end text-red-600 line-through">
                 ${(price / (1 - discountPercentage / 100)).toFixed(2)}
+              </span>
+              <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
+                {rating}
               </span>
             </p>
             <div className="flex items-center">
               <StarRating rating={rating} />
-              <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
-                {rating}
-              </span>
             </div>
           </div>
           <div className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 cursor-pointer">
