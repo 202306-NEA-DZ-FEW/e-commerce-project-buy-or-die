594e9975c557ad77c4c818fadc745ec01ef95d50
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-09-28 01:05:19 +0200

    feat(products): implement filter and sort functionality
    - Added filter options for Name, Price, and Rating. Implemented sorting based on the selected
    filter.
    
    Closes #24

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,5 +1,5 @@
 import LeftSideBar from "@/components/Filter/LeftSideBar"
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { useRouter } from "next/router"
 import { fetcher } from "@/Utils/API"
 import ProductCard from "@/components/Cards/ProductCard"
@@ -7,19 +7,88 @@ import Link from "next/link"
 
 const Products = ({ produ }) => {
   const [isFilterDrawerOpen, setIsFilterDrawerOpen] = useState(false)
-  const [selectedfilter, setSelectedfilter] = useState(null)
+  const [selectedFilter, setSelectedFilter] = useState(null)
+  const [selectedFilterType, setSelectedFilterType] = useState(null)
   const router = useRouter()
-  const { category } = router.query
-  const filteredProducts = category
-    ? produ.products.filter((prods) => prods.category === category)
-    : produ.products
+  const { category, filter } = router.query
+  const filterDrawerRef = useRef(null)
 
-  const handleCategoryClick = (filter) => {
-    router.push({
-      pathname: router.pathname,
-      query: { filter },
-    })
-    setSelectedfilter(filter)
+  const handleCategoryClick = (filterType) => {
+    let updatedQuery = { pathname: router.pathname }
+
+    if (
+      filterType === "Name" ||
+      filterType === "Price" ||
+      filterType === "Rating"
+    ) {
+      updatedQuery.query = { ...router.query, filter: filterType }
+    } else {
+      updatedQuery.query = { ...router.query, filter: null }
+    }
+
+    router.push(updatedQuery)
+    setSelectedFilterType(filterType)
+    setIsFilterDrawerOpen(!isFilterDrawerOpen)
+  }
+
+  useEffect(() => {
+    setSelectedFilter(filter)
+  }, [filter])
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        filterDrawerRef.current &&
+        !filterDrawerRef.current.contains(event.target)
+      ) {
+        setIsFilterDrawerOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+
+  const filterProducts = () => {
+    if (selectedFilterType === "Name") {
+      return produ.products
+        .slice()
+        .sort((a, b) => a.title.localeCompare(b.title))
+    } else if (selectedFilterType === "Price") {
+      return produ.products.slice().sort((a, b) => a.price - b.price)
+    } else if (selectedFilterType === "Rating") {
+      return produ.products.slice().sort((a, b) => b.rating - a.rating)
+    } else {
+      return produ.products
+    }
+  }
+
+  let filteredProducts = produ.products
+
+  if (filter) {
+    filteredProducts = filterProducts()
+  }
+
+  if (category) {
+    filteredProducts = produ.products.filter(
+      (prods) => prods.category === category,
+    )
+
+    if (selectedFilterType) {
+      filteredProducts = filteredProducts.slice().sort((a, b) => {
+        if (selectedFilterType === "Name") {
+          return a.title.localeCompare(b.title)
+        } else if (selectedFilterType === "Price") {
+          return a.price - b.price
+        } else if (selectedFilterType === "Rating") {
+          return b.rating - a.rating
+        }
+        return 0
+      })
+    }
   }
 
   const toggleFilterDrawer = () => {
@@ -34,7 +103,7 @@ const Products = ({ produ }) => {
         <div class="col-span-2 md:col-span-1 lg:col-span-0 ">
           <div class="bg-gray-300 p-10 h-full ">Sidebar</div>
         </div>
-        <div class="col-span-2 md:col-span-2 lg:col-span-3 ">
+        <div class="col-span-2 md:col-span-2 lg:col-span-3 mt-10 ">
           <div className="container grid grid-cols-2 p-2 text-black rounded-full ">
             <div className="menu col-span-2 md:col-span-1 lg:col-span-1 ml-20 ">
               <div className="title">
@@ -43,32 +112,37 @@ const Products = ({ produ }) => {
                 </p>
               </div>
             </div>
-            <div className="filter col-span-2 md:col-span-1 lg:col-span-1">
+            <div className="filter col-span-2 md:col-span-1 lg:col-span-1 ">
               <div className="flex  items-center text-black justify-end space-x-2">
-                <div className="mr-10">
+                <div className="">
                   Sort by :{" "}
                   <button className="font-bold" onClick={toggleFilterDrawer}>
-                    {selectedfilter ? selectedfilter : "Filter"}
+                    {selectedFilter ? selectedFilter : "Filter"}
                   </button>
                 </div>
                 {isFilterDrawerOpen && (
-                  <div className=" absolute mt-40  bg-white text-black z-20 ">
-                    <div className="mb-4">
-                      {" "}
-                      <button onClick={() => handleCategoryClick("Title 1")}>
-                        Title 1
+                  <div
+                    className="absolute mt-36 bg-white text-black z-20"
+                    ref={filterDrawerRef}
+                  >
+                    <div className="mb-4 ">
+                      <button onClick={() => handleCategoryClick("All")}>
+                        All
+                      </button>
+                    </div>
+                    <div className="mb-4 ">
+                      <button onClick={() => handleCategoryClick("Name")}>
+                        Name
                       </button>
                     </div>
                     <div className="mb-4">
-                      {" "}
-                      <button onClick={() => handleCategoryClick("Title 2")}>
-                        Title 2
+                      <button onClick={() => handleCategoryClick("Price")}>
+                        Price
                       </button>
                     </div>
                     <div className="mb-4">
-                      {" "}
-                      <button onClick={() => handleCategoryClick("Title 3")}>
-                        Title 3
+                      <button onClick={() => handleCategoryClick("Rating")}>
+                        Rating
                       </button>
                     </div>
                   </div>
