4bee6dcd6b36634b352654449ac817f9fc91682c
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-09-29 21:45:29 +0100

    feat(add randomimg): add RandomImage
    added Random image div to the home page
    
    #40

--- src/components/Cards/RandomImg.jsx
+++ src/components/Cards/RandomImg.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { fetcher } from "@/Utils/API"
-import HomeCard from "./HomeCards"
 
 const RandomImages = () => {
   const [imageUrls, setImageUrls] = useState([])
+  const [currentImages, setCurrentImages] = useState([null, null, null])
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -31,7 +32,71 @@ const RandomImages = () => {
     fetchImages()
   }, [])
 
-  return <div>{imageUrls.length > 0 && <HomeCard {...{ imageUrls }} />}</div>
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const newImages = [...currentImages]
+      newImages[currentIndex] =
+        imageUrls[Math.floor(Math.random() * imageUrls.length)]
+      setCurrentImages(newImages)
+      setCurrentIndex((currentIndex + 1) % 3)
+    }, 2000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [currentImages, currentIndex, imageUrls])
+
+  return (
+    <div className="absolute top-[1728px] left-[144px] bg-whitesmoke w-[1440px] h-[1024px] overflow-hidden text-13xl">
+      <div className="absolute top-[8px] left-[589px] flex flex-col items-center justify-start gap-[12px]">
+        <b className="relative">Categories</b>
+        <div className="relative text-lg font-medium text-color-21">
+          Find what you are looking for
+        </div>
+      </div>
+      <div className="absolute top-[191px] left-[0px] bg-color-1 w-[1440px] h-[841px] text-lg text-color-2">
+        <div className="absolute top-[-48px] left-[96px] flex flex-col items-center justify-start gap-[12px]">
+          <img
+            className="relative rounded-xl w-[352px] h-[512px] overflow-hidden shrink-0 object-cover"
+            alt=""
+            src={currentImages[0]}
+          />
+          <b className="relative">text</b>
+        </div>
+        <div className="absolute top-[-48px] left-[992px] flex flex-col items-center justify-start gap-[12px]">
+          <img
+            className="relative rounded-xl w-[352px] h-[512px] overflow-hidden shrink-0 object-cover"
+            alt=""
+            src={currentImages[1]}
+          />
+          <b className="relative">text</b>
+        </div>
+        <div className="absolute top-[48px] left-[544px] flex flex-col items-center justify-start gap-[24px]">
+          <div className="flex flex-col items-start justify-start gap-[12px]">
+            <div className="flex flex-col items-center justify-start gap-[12px]">
+              <img
+                className="relative rounded-xl w-[352px] h-[512px] overflow-hidden shrink-0 object-cover"
+                alt=""
+                src={currentImages[2]}
+              />
+              <b className="relative">text</b>
+            </div>
+            <div className="relative font-medium text-color-21 text-center inline-block w-[352px]">
+              text
+            </div>
+          </div>
+          <div className="rounded-lg bg-white flex flex-row items-center justify-start py-3 px-6 gap-[10px] text-center">
+            <div className="relative font-medium">Explore</div>
+            <img
+              className="relative w-6 h-5 overflow-hidden shrink-0"
+              alt=""
+              src="/arrowright.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default RandomImages
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 import Carousels from "@/components/Carousel"
+import RandomImages from "@/components/Cards/RandomImg"
 
 const HomePage = () => {
   return (
     <div>
       <Carousels />
+
+      <RandomImages />
     </div>
   )
 }
